ce3f29868ede8b5d82a225ae753bd2f1e3b1b07e
cam <cchumbemalarin@gmail.com>
2025-09-04 09:32:33 -0500

    Aplicación del método getElementById

--- Sesión 33/assets/js/script.js
+++ Sesión 33/assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("Hola Mundo");
+// Métodos del objeto "document" para seleccionar elementos del DOM. Los elementos retornado pueden ser almacenados en variables.
+
+// getElementById("nombre-id") -> Selecciona el elemento con el id "nombre-id".
+let mainTitle = document.getElementById("main-title");
+console.log(mainTitle);
+
+//Acceder al contenido con el atributo "innerText"
+console.log(mainTitle.innerText);
+
+//Actualizamod rl contenido desde JS
+mainTitle.innerText = "TÍTULO CAMBIADO";
+console.log(mainTitle.innerText);
