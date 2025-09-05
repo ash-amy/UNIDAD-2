94ca83dc2139aca1fe934532b3ee26a756099703
cam <cchumbemalarin@gmail.com>
2025-09-05 09:09:46 -0500

    Aplicación de los Métodos PUSH y POP

--- Sesión 34/assets/js/script.js
+++ Sesión 34/assets/js/script.js
@@ -1,1 +1,16 @@
-console.log("Hola Mundo");
+console.log("MÉTODOS DE ARRAYS");
+//nombreArray.metodo()
+
+let fruits = ["manzana", "naranja", "pera", "mango"];
+console.log(fruits);
+
+console.log("PUSH");
+//push(x) -> Agrega el elemento x añ FINAL del array.
+fruits.push("fresa");
+console.log(fruits);
+
+console.log("POP");
+//pop() -> Elimina el ÚLTIMO elemento del array y lo retorna.
+fruits.pop();
+console.log(fruits);
+
